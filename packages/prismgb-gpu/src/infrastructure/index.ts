407d5ef5d96c328361e25877e038a02b4ed838ea
josstei <48696594+josstei@users.noreply.github.com>
2026-02-05 00:06:03 -0500

    feat(gpu): add infrastructure barrel export

---
+++ packages/prismgb-gpu/src/infrastructure/index.ts
@@ -0,0 +1,4 @@
+export { BasePipeline, type BasePipelineConfig } from './base-pipeline';
+export { Canvas2DPipeline } from './canvas2d';
+export { WebGL2Pipeline } from './webgl2';
+export { WebGPUPipeline } from './webgpu';
